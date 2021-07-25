421328043e3d8c30e073c6aa52d6807c6057d93f
drewmrodriguez <drewmrodriguez@gmail.com>
2021-07-25 12:35:00 -0700

    first commit

--- api/server.js
+++ api/server.js
@@ -1,7 +1,13 @@
-const express = require("express")
+const express = require('express');
+const helmet = require('helmet');
 
-const server = express()
+const carsRouter = require('./cars/cars-router');
 
-// DO YOUR MAGIC
+const server = express();
 
-module.exports = server
+server.use(helmet());
+server.use(express.json());
+
+server.use('/api/cars', carsRouter);
+
+module.exports = server;
